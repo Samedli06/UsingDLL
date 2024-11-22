bf7f1508148c8fc703fb45950a87287c1b35c93b
Bakhtiyar <Bakhtiyar@LAPTOP-SJO69TLI>
2024-11-22 18:58:02 +0400

    Add project files.

---
+++ Test/Program.cs
@@ -0,0 +1,17 @@
+﻿using MyCustomCalculator;
+
+namespace Test
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+          
+
+            Calculator calculator = new Calculator();
+
+            double x = calculator.Add(3, 6);
+            Console.WriteLine(x);
+        }
+    }
+}
